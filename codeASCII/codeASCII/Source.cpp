785edcbf72a31c0eb58bd364bf476416ea729062
Congloi33 <72296076+Congloi33@users.noreply.github.com>
2021-08-01 20:39:38 +0700

    Add files via upload

---
+++ codeASCII/codeASCII/Source.cpp
@@ -0,0 +1,16 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+#include "windows.h"
+int main() {
+    std::fstream f;
+    f.open("C:/Users/Admin/Pictures/meme/to.txt ");// thay đường dẫn này bằng đường dẫn của bạn
+    std::string line;
+    while (!f.eof()) {
+        getline(f, line);
+        std::cout << line << std::endl;
+        Sleep(200);// độ nghỉ 
+    }
+    f.close();
+    return 0;
+}
